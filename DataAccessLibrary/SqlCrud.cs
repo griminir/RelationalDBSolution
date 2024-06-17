2277bfab5ee324d4e6ad0591c92a4d154074acf0
griminir <viktor.elliot.king@gmail.com>
2024-06-18 00:57:20 +0200

    Add project files.

---
+++ DataAccessLibrary/SqlCrud.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLibrary.Models;
+
+namespace DataAccessLibrary
+{
+    public class SqlCrud
+    {
+        private readonly string _connectionString;
+        private SqlDataAccess db = new SqlDataAccess();
+
+        public SqlCrud(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<BasicContactModel> GetAllContacts()
+        {
+            string sql = "select Id, FirstName, LastName from dbo.Contacts";
+
+            return db.LoadData<BasicContactModel, dynamic>(sql, new { }, _connectionString);
+        }
+
+        public FullContactModel GetFullContactById(int id)
+        {
+            string sql = $"select Id, FirstName, LastName from dbo.Contacts where Id = @Id";
+            FullContactModel output = new FullContactModel();
+
+            output.BasicInfo = db.LoadData<BasicContactModel, dynamic>(sql, new { Id = id }, _connectionString).FirstOrDefault();
+
+            if (output.BasicInfo == null)
+            {
+                // do something here to tell user record was not found
+                //throw new Exception("user not found");
+                return null;
+            }
+
+            sql = @"select e.*
+                    from dbo.EmailAddresses e
+                    inner join
+                    dbo.ContactEmail ce on ce.EmailAddressId = e.Id
+                    where ce.ContactId = @Id";
+
+            output.EmailAddresses = db.LoadData<EmailAddressModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            sql = @"select p.*
+                    from dbo.PhoneNumbers p
+                    inner join
+                    dbo.ContactPhoneNumbers cpn on cpn.PhoneNumberId = p.Id
+                    where cpn.ContactId = @Id";
+
+            output.PhoneNumbers = db.LoadData<PhoneNumberModel, dynamic>(sql, new { Id = id }, _connectionString);
+
+            return output;
+        }
+
+        public void CreateContact(FullContactModel contact)
+        {
+            //save the basic contact
+            var sql = "insert into dbo.Contacts (FirstName, LastName) values (@FirstName, @LastName);";
+            db.SaveData(sql,
+                new {contact.BasicInfo.FirstName, contact.BasicInfo.LastName}, // same as FirstName = contact.x.y
+                _connectionString);
+
+            //get the ID number of the contact
+            sql = "select Id from dbo.Contacts where FirstName = @FirstName and LastName = @LastName;";
+            var contactId = db.LoadData<IdLookUpModel, dynamic>(
+                sql, 
+                new { contact.BasicInfo.FirstName, contact.BasicInfo.LastName },
+                _connectionString).First().Id;
+
+            
+            foreach (var phoneNumber in contact.PhoneNumbers)
+            {
+                if (phoneNumber.Id == 0)
+                {
+                    sql = "insert into dbo.PhoneNumbers (PhoneNumber) values (@PhoneNumber);";
+                    db.SaveData(sql, new {phoneNumber.PhoneNumber}, _connectionString);
+
+                    sql = "select Id from dbo.PhoneNumbers where PhoneNumber = @PhoneNumber;";
+                    phoneNumber.Id =
+                        db.LoadData<IdLookUpModel, dynamic>(
+                            sql, 
+                            new { phoneNumber.PhoneNumber },
+                            _connectionString).First().Id;
+                }
+
+                sql = "insert into dbo.ContactPhoneNumbers (ContactId, PhoneNumberId) values (@ContactId, @PhoneNumberId);";
+                db.SaveData(sql, new { ContactId = contactId, PhoneNumberId = phoneNumber.Id }, _connectionString);
+                //the new { x = y} is because we can not infer it like we have done before, so we need to specify 
+            }
+
+            foreach (var email in contact.EmailAddresses)
+            {
+                if (email.Id == 0)
+                {
+                    sql = "insert into dbo.EmailAddresses (EmailAddress) values (@EmailAddress);";
+                    db.SaveData(sql, new {email.EmailAddress}, _connectionString);
+
+                    sql = "select Id from dbo.EmailAddresses where EmailAddress = @EmailAddress;";
+                    email.Id =
+                        db.LoadData<IdLookUpModel, dynamic>(
+                                sql, 
+                                new { email.EmailAddress }, 
+                                _connectionString).First().Id;
+                }
+
+                sql = "insert into dbo.ContactEmail (ContactId, EmailAddressId) values (@ContactId, @EmailAddressId);";
+                db.SaveData(sql, new {ContactId = contactId, EmailAddressId  = email.Id}, _connectionString);
+            }
+            
+        }
+
+        public void UpdateContactName(BasicContactModel contact)
+        {
+            var sql = "Update dbo.Contacts set FirstName = @FirstName, LastName = @LastName where Id = @Id";
+            db.SaveData(sql, contact, _connectionString);
+        }
+
+        public void DeletePhoneNumber(int contactId, int phoneNumberId)
+        {
+            var sql = "select Id, ContactId, PhoneNumberId from dbo.ContactPhoneNumbers where PhoneNumberId = @PhoneNumberId;";
+            var links = db.LoadData<ContactPhoneNumberModel, dynamic>(
+                sql, 
+                new { PhoneNumberId = phoneNumberId },
+                _connectionString);
+
+            sql = "delete from dbo.ContactPhoneNumbers where PhoneNumberId = @PhoneNumberId and ContactId = @ContactId;";
+            db.SaveData(sql, new {PhoneNumberId = phoneNumberId, ContactId = contactId}, _connectionString);
+            
+            if (links.Count == 1)
+            {
+                sql = "delete from dbo.PhoneNumbers where Id = @PhoneNumberId;";
+                db.SaveData(sql, new { PhoneNumberId = phoneNumberId }, _connectionString);
+            }
+        }
+    }
+}
---
+++ SQLServerUI/Program.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Extensions.Configuration;
+using DataAccessLibrary;
+using DataAccessLibrary.Models;
+
+namespace SQLServerUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlCrud sql = new SqlCrud(GetConnectionString());
+
+            //ReadAllContacts(sql);
+
+            //ReadContact(sql, 3);
+
+            //CreateNewContact(sql);
+
+            //UpdateContact(sql);
+
+            //RemovePhoneNumberFromContact(sql, 1, 2);
+
+            Console.WriteLine("done processing");
+            Console.ReadLine();
+        }
+
+        private static void UpdateContact(SqlCrud sql)
+        {
+            BasicContactModel contact = new BasicContactModel
+            {
+                Id = 1,
+                FirstName = "Timmy",
+                LastName = "storm"
+            };
+            sql.UpdateContactName(contact);
+
+        }
+        private static void RemovePhoneNumberFromContact(SqlCrud sql, int contactId, int phoneNumberId)
+        {
+            sql.DeletePhoneNumber(contactId, phoneNumberId);
+        }
+        private static void CreateNewContact(SqlCrud sql)
+        {
+            FullContactModel user = new FullContactModel
+            {
+                BasicInfo = new BasicContactModel
+                {
+                    FirstName = "Sue",
+                    LastName = "Storm"
+                }
+            };
+
+            user.EmailAddresses.Add(new EmailAddressModel { EmailAddress = "sue@gmail.com" });
+            user.EmailAddresses.Add(new EmailAddressModel { Id = 2, EmailAddress = "me@gmail.com" });
+
+            user.PhoneNumbers.Add(new PhoneNumberModel { Id = 2, PhoneNumber = "40400404" });
+            user.PhoneNumbers.Add(new PhoneNumberModel { PhoneNumber = "55555555" });
+
+            sql.CreateContact(user);
+        }
+        private static void ReadAllContacts(SqlCrud sql)
+        {
+            var rows = sql.GetAllContacts();
+
+            foreach (var row in rows)
+            {
+                Console.WriteLine($"{row.Id}: {row.FirstName} {row.LastName}");
+            }
+        }
+        private static void ReadContact(SqlCrud sql, int contactId)
+        {
+            var contact = sql.GetFullContactById(contactId);
+
+            Console.WriteLine($"{contact.BasicInfo.Id} {contact.BasicInfo.FirstName} {contact.BasicInfo.LastName}");
+        }
+        private static string GetConnectionString(string ConnectionStringName = "Default")
+        {
+            var output = "";
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+
+            var config = builder.Build();
+
+            output = config.GetConnectionString(ConnectionStringName);
+
+            return output;
+        }
+    }
+}
